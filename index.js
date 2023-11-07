7872a6899c32b1462c8a9461c7ee8ea8a0261500
varunkumar227 <varunkumarreddy227@gmail.com>
2023-11-07 15:12:40 -0500

    localstorage

--- index.js
+++ index.js
@@ -1,66 +1,76 @@
 var addButton = document.getElementById("addTask");
+var searchInput = document.getElementById("search");
+var taskList = document.getElementById("taskList");
+
+var tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function updateTaskList() {
+    taskList.innerHTML = "";
+    tasks.forEach(function (task, index) {
+        var taskItem = document.createElement("li");
+        taskItem.className = "task-item";
+        taskItem.innerHTML = "<b>" + task.name + "</b> - <strong>" + task.taskName + "</strong> - " + task.description + " for " + task.taskDuration + " " + task.durationUnit + " finish before " + task.deadLine + "  <button class='delete-button' onclick='deleteTask(" + index + ")'> Delete </button> <button class='edit-button' onclick='editTask(" + index + ")'> Edit </button>";
+        taskList.appendChild(taskItem);
+    });
+}
+
+updateTaskList();
 
 addButton.addEventListener("click", function() {
     addTask();
 });
 
 function addTask() {
-    var taskName = document.getElementById("taskName").value.trim();
-    var name = document.getElementById("name").value.trim();
-    var description = document.getElementById("description").value.trim();
+    var name = document.getElementById("name").value;
+    var taskName = document.getElementById("taskName").value;
+    var description = document.getElementById("description").value;
     var deadLine = document.getElementById("deadLine").value;
-    var taskDuration = document.getElementById("taskDuration").value.trim();
+    var taskDuration = document.getElementById("taskDuration").value;
     var durationUnit = document.getElementById("durationUnit").value;
-    if (taskName === "" || name === "" || description === "" || taskDuration === "" || deadLine === "") {
-        alert("Task details cannot be empty!");
-        return;
-    }
 
-    if (isNaN(parseFloat(taskDuration))) {
-        alert("Task duration must be a numerical value!");
-        return;
-    }
+    tasks.push({
+        name: name,
+        taskName: taskName,
+        description: description,
+        deadLine: deadLine,
+        taskDuration: taskDuration,
+        durationUnit: durationUnit,
+    });
 
-    var taskList = document.getElementById("taskList");
-    var taskItem = document.createElement("li");
-    taskItem.className = "task-item";
-    taskItem.innerHTML = `<b>${name}</b> - <strong>${taskName}</strong> - ${description} for ${taskDuration} ${durationUnit} finish before ${deadLine}  <button class="delete-button" onclick="deleteTask(this)"> Delete </button> <button class="edit-button" onclick="editTask(this)"> Edit </button>`;
-    taskList.appendChild(taskItem);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 
     // Clear the input fields
-    document.getElementById("taskName").value = "";
     document.getElementById("name").value = "";
+    document.getElementById("taskName").value = "";
     document.getElementById("description").value = "";
     document.getElementById("deadLine").value = "";
     document.getElementById("taskDuration").value = "";
+
+    updateTaskList();
 }
 
-function deleteTask(buttonElement) {
-    var taskList = document.getElementById("taskList");
-    var taskItem = buttonElement.parentElement;
-    taskList.removeChild(taskItem);
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    updateTaskList();
 }
 
-function editTask(buttonElement) {
-    var taskItem = buttonElement.parentElement;
+function editTask(index) {
+    // Edit the task at the specified index
+    // ...
 
-    // Extract the details from the task item
-    var name = taskItem.querySelector("b").textContent;
-    var taskName = taskItem.querySelector("strong").textContent;
-    var description = taskItem.textContent.split('-')[2].split('for')[0].trim();
-    var taskDuration = taskItem.textContent.match(/\d+\s\w+/)[0].split(' ')[0];
-    var durationUnit = taskItem.textContent.match(/\d+\s\w+/)[0].split(' ')[1];
-    var deadLine = taskItem.textContent.match(/\d{4}-\d{2}-\d{2}/)[0];
-
-    // Populate the input fields with the existing details for editing
-    document.getElementById("taskName").value = taskName;
-    document.getElementById("name").value = name;
-    document.getElementById("description").value = description;
-    document.getElementById("deadLine").value = deadLine;
-    document.getElementById("taskDuration").value = taskDuration;
-    document.getElementById("durationUnit").value = durationUnit;
-
-    // Remove the existing task item
-    var taskList = document.getElementById("taskList");
-    taskList.removeChild(taskItem);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    updateTaskList();
 }
+
+searchInput.addEventListener("input", function() {
+    var searchTerm = searchInput.value.toLowerCase();
+    var filteredTasks = tasks.filter(function(task) {
+        return task.name.toLowerCase().includes(searchTerm) || task.taskName.toLowerCase().includes(searchTerm);
+    });
+    taskList.innerHTML = "";
+    filteredTasks.forEach(function (task, index) {
+        // Display filtered tasks
+        // ...
+    });
+});
