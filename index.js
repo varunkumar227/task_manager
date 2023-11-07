ea91f1142b92ce0d50f3c5ccb9b5e4eeb701ba65
varunkumar227 <varunkumarreddy227@gmail.com>
2023-11-06 22:06:55 -0500

    css

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ addButton.addEventListener("click", function() {
     addTask();
 });
 function addTask() {
-    var taskName = document.getElementById("taskName").value;
-    var name = document.getElementById("name").value;
-    var description = document.getElementById("description").value;
-    var taskDuration = document.getElementById("taskDuration").value;
+    var taskName = document.getElementById("taskName").value.trim();
+    var name = document.getElementById("name").value.trim();
+    var description = document.getElementById("description").value.trim();
+    var taskDuration = document.getElementById("taskDuration").value.trim();
     var durationUnit = document.getElementById("durationUnit").value;
     if (taskName === "" ||name === "" || taskDuration === "") {
         alert("Task details cannot be empty!");
         return;
     }
 
+    if (isNaN(parseFloat(taskDuration))) {
+        alert("Task duration must be a numerical value!");
+        return;
+    }
+    
     var taskList = document.getElementById("taskList");
     var taskItem = document.createElement("li");
     taskItem.className = "task-item";
